c39cfff09bf66e9c0c7a24d2028b5538c46ecb53
banahaker <pn0818x@outlook.com>
2023-01-31 17:57:42 +0800

    feat: algorithms

---
+++ algorithm.py
@@ -0,0 +1,23 @@
+def GCD(x, y):
+  if x > y :
+    c = x
+  else:
+      c = y
+  for k in range(c, 2, -1):
+      if x % k == 0 and y % k == 0:
+          return k
+  return 1
+
+def isPrime(n):
+  c = 2
+  while c <= n:
+    if n%c == 0:
+      return True
+    c+=1
+
+  return False
+
+def lazpPleaseYou(cupA, cupB):
+  if cupA >= cupB:
+    return cupA
+  return cupB
